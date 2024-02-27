4daafbbcf160005bbf3e1a3ce225eb5540b0297d
Zaouich123 <etienne.baillie@gmail.com>
2024-02-27 15:08:23 +0100

    Creating router and more for fun

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/Telephone',
+      name: 'Telephone',
+      component: () => import('../views/TelephoneView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/Tablette',
+      name: 'Tablette',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/TabletteView.vue')
+    },
+    {
+      path: '/Forfait',
+      name: 'Forfait',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ForfaitView.vue')
     }
   ]
 })
