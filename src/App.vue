85b5465c573102b5ecd7751ffbe853439b0fc941
darkTravus <tchira644@gmail.com>
2024-02-27 16:19:45 +0100

    Adding the process of "productList"

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/categories/Minecraft">Minecraft Category</RouterLink>
+        <RouterLink to="/categories/Tablette">Tablette</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/ProductListView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: ['categoryName'], // Définissez le nom de la prop correspondant au paramètre
+};
+</script>
+
+<template>
+    <div class="about">
+      <h1>List of {{ categoryName }}</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
