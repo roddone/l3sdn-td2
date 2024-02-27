5c28c0333cdf46a9df5e325544bcd403e15e8bed
Zaouich123 <etienne.baillie@gmail.com>
2024-02-27 15:40:02 +0100

    App change for navbar

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Forfait">Forfait</RouterLink>
+        <RouterLink to="/Tablette">Tablette</RouterLink>
+        <div>
+          <RouterLink to="/Telephone">Telephone</RouterLink>
+          <RouterLink to="/Tablette">???????</RouterLink>
+        </div>
       </nav>
     </div>
   </header>
