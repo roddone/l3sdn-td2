3defd5234c20a0eba6cf627e6be8bcdedf069fd3
Alban Pruvost <albanpruvost@MacBook-Pro-de-Alban.local>
2024-02-27 18:04:13 +0100

    add shop

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// index.js
+import { createRouter, createWebHistory } from 'vue-router';
+
+import ProductListView from '../views/ProductListView.vue';
+import ShoppingCart from '../views/ShoppingCart.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', component: Home },
     {
       path: '/categories/:categoryName',
       name: 'productList',
-      
-      component: () => import('../views/ProductListView.vue'),
-      props: true, 
-    }
-  ]
-})
+      component: ProductListView,
+      props: true,
+    },
+    { path: '/panier', component: ShoppingCart, name: 'shoppingCart' },
+  ],
+});
 
-export default router
+export default router;
