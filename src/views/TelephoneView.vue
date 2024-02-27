26fb2ddcdc29faf25daedb2d923345bb042d0fce
Vianney Lehu <60597553+RicqPascalv2@users.noreply.github.com>
2024-02-27 16:36:19 +0100

    template in all view

---
+++ src/views/TelephoneView.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div>
+        <b-nav vertical class="w-25">
+        <b-nav-item>Iphone</b-nav-item>
+        <b-nav-item>Samsung</b-nav-item>
+        <b-nav-item>Lenovo</b-nav-item>
+        <b-nav-item>One plus</b-nav-item>
+        </b-nav>
+    </div>
+
+</template>
