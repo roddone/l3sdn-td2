692a70a304cf4d6d4d3a8f5fc51e27f61f99a253
ArslaneDevil <arslaneben08@gmail.com>
2024-02-27 17:27:23 +0100

    product list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HomePage from './components/HomePage.vue'
-</script>
-
 <template>
-
-  <HomePage/>
-  
-  
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,7 +2,7 @@
     <div>
       <h1>Product List</h1>
       <ul>
-        <li v-for="product in products" :key="product.id">
+        <li v-for="product in filteredProducts" :key="product.id">
           <button @click="goToProductDetails(product.id)">
             {{ product.name }}
           </button>
@@ -12,16 +12,26 @@
   </template>
   
   <script setup>
-  import { ref } from 'vue';
-  import { useRouter, useRoute } from 'vue-router';
-  
-  const router = useRouter();
-  const route = useRoute();
-  const category = route.params.category;
-  const products = ref([]); 
-  
-  function goToProductDetails(productId) {
-    router.push(`/product/${productId}`);
-  }
-  </script>
+import { ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+const category = route.params.category;
+const products = ref([
+  { id: 1, name: 'iPhone 12', category: 'telephones' },
+  { id: 2, name: 'Samsung Galaxy S21', category: 'telephones' },
+  { id: 3, name: 'Google Pixel 5', category: 'telephones' },
+  { id: 4, name: 'OnePlus 9', category: 'telephones' },
+]); 
+
+
+
+const filteredProducts = ref(products.value.filter(product => product.category === category));
+
+function goToProductDetails(productId) {
+  router.push(`/product/${productId}`);
+}
+</script>
+
   
