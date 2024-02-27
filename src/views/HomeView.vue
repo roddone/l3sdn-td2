65dc22d95e572350188546e51cd7cbffe3ee9f56
Sacha Duvivier <sduvivier@walibuy.com>
2024-02-27 16:03:49 +0100

    first base

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <h1>Nos produits</h1>
+      <div class="flex gap-8">
+            <RouterLink class="bg-red-300 flex-1 p-8 text-black" to="/product/canape">Canapé</RouterLink>
+            <RouterLink class="bg-blue-300 flex-1 p-8 text-black" to="/product/pergola">Pergola</RouterLink>
+            <RouterLink class="bg-orange-300 flex-1 p-8 text-black" to="/product/chaise">Chaise</RouterLink>
+      </div>
+    </div>
   </main>
 </template>
---
+++ src/views/ProductListView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
+const type = route.params.type;
+
+</script>
+<template>
+  <div class="about">
+    <div v-if="type === 'toto'">TOto</div>
+    <h1>This is an product page</h1>
+  </div>
+</template>
+
+<style>
+</style>
