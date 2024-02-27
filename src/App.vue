4b8e07232f536c5070101eae2c373322bbbc603c
theo dadon <theo.td49@gmail.com>
2024-02-27 17:01:47 +0100

    navbar + app et maj tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <Menu />
-  <router-view />
+  <div>
+    <nav class="bg-gray-800 text-white p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <router-link to="/menu" class="text-lg font-bold">Menu</router-link>
+        <div>
+          <router-link to="/telephones" class="mr-4">Téléphones</router-link>
+          <router-link to="/tablettes" class="mr-4">Tablettes</router-link>
+          <router-link to="/forfaits" class="mr-4">Forfaits</router-link>
+          <router-link to="/marques" class="mr-4">Marques</router-link>
+          <router-link to="/panier" class="relative">
+            Panier
+            <span v-if="panier.length" class="absolute top-0 right-0 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-xs">{{ panier.length }}</span>
+          </router-link>
+        </div>
+      </div>
+    </nav>
+    <div class="py-5">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
-import Menu from './components/Menu.vue';
+import { computed } from 'vue';
+import { useStore } from '@/stores/index.js';
 
 export default {
-  components: {
-    Menu
+  setup() {
+    const store = useStore();
+    const panier = computed(() => store.panier);
+    return { panier };
   }
 };
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,26 @@
 <template>
-    <nav class="bg-gray-800 text-white p-4 flex justify-between">
-      <div>
-        <router-link to="/" class="hover:underline text-xl font-bold">MonApp</router-link>
-      </div>
-      <div class="flex space-x-4">
-        <router-link to="/telephones" class="hover:underline">Téléphones</router-link>
-        <router-link to="/tablettes" class="hover:underline">Tablettes</router-link>
-        <router-link to="/forfaits" class="hover:underline">Forfaits</router-link>
-        <router-link to="/marques" class="hover:underline">Marques</router-link>
-        <router-link to="/panier" class="relative hover:underline">
+    <nav class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <router-link to="/" class="text-lg font-bold">Mon Magasin</router-link>
+      <div class="space-x-4">
+        <router-link to="/telephones">Téléphones</router-link>
+        <router-link to="/tablettes">Tablettes</router-link>
+        <router-link to="/forfaits">Forfaits</router-link>
+        <router-link to="/marques">Marques</router-link>
+        <router-link to="/panier" class="relative">
           Panier
-          <span v-if="panierCount > 0" class="absolute top-0 right-0 block h-2 w-2 rounded-full bg-red-500"></span>
+          <span v-if="panierCount > 0" class="absolute top-0 right-0 block h-6 w-6 bg-red-500 text-center rounded-full text-xs">{{ panierCount }}</span>
         </router-link>
       </div>
     </nav>
   </template>
   
   <script>
-  import { useStore } from '../store';
+  import { useStore } from '@/stores/index.js';
   
   export default {
     setup() {
       const store = useStore();
       const panierCount = store.panierCount;
-  
       return { panierCount };
     }
   };
