b94cda8b9414f751e005dfbafe95749287750c0f
Lucas Maveau <lucas.maveau@gmail.com>
2024-02-27 17:35:30 +0100

    modif router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Telephone from '../components/Telephone.vue';
-import Panier from '../components/Panier.vue';
-import Tablette from '../components/Tablette.vue';
-import Forfait from '../components/Forfait.vue';
-import Marque from '../components/Marque.vue';
+import Telephones from '../components/Telephone.vue';
+import Paniers from '../components/Panier.vue';
+import Tablettes from '../components/Tablette.vue';
+import Forfaits from '../components/Forfait.vue';
+import Marques from '../components/Marque.vue';
 import Menu from '../components/Menu.vue';
 
 const routes = [
-  { path: '/telephones', component: Telephone },
-  { path: '/panier', component: Panier },
-  { path: '/tablettes', component: Tablette },
-  { path: '/forfaits', component: Forfait },
-  { path: '/marques', component: Marque },
-  { path: '/menu', component: Menu },
+  { path: '/telephones', component: Telephones, name : 'telephones'},
+  { path: '/panier', component: Paniers, name : 'panier'},
+  { path: '/tablettes', component: Tablettes, name : 'tablettes'},
+  { path: '/forfaits', component: Forfaits, name : 'forfaits'},
+  { path: '/marques', component: Marques, name : 'marques'},
+  { path: '/menu', component: Menu, name : 'menu'},
   { path: '/', redirect: '/menu' },
 ];
 
