03e7048ccf7cdb6a8b41e0232f22cd1e5318f32e
Vianney Lehu <vianney.lehu@lacatholille.fr>
2024-03-01 13:38:42 +0100

    implementing display products

--- src/views/TelephoneView.vue
+++ src/views/TelephoneView.vue
@@ -1,12 +1,113 @@
 <template>
-
-    <div>
-        <b-nav vertical class="w-25">
-        <b-nav-item>Iphone</b-nav-item>
-        <b-nav-item>Samsung</b-nav-item>
-        <b-nav-item>Lenovo</b-nav-item>
-        <b-nav-item>One plus</b-nav-item>
-        </b-nav>
+  <div>
+    <div v-for="category in items" :key="category.label">
+      <h2>{{ category.label }}</h2>
+      <ul>
+        <li v-for="item in category.items" :key="item.id" @click="handleClick(item)">
+          {{ item.label }}
+        </li>
+      </ul>
     </div>
-
+  </div>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+import { ref } from "vue";
+
+const items = ref([
+    {
+        label: 'Iphone',
+        items : [
+            {
+                label:'Iphone 9',
+                type:'telephone',
+                id :'1'
+            },
+            {
+                label: 'Iphone 10',
+                type:'telephone',
+                id :'2'
+            },
+            {
+                label: 'Iphone 11',
+                type:'telephone',
+                id :'3'
+            }
+
+        ]
+    },
+    {
+        label: 'Samsung',
+
+        items : [
+            {
+                label:'Samsung 9',
+                type:'telephone',
+                id :'4'
+            },
+            {
+                label: 'Samsung 10',
+                type:'telephone',
+                id :'5'
+            },
+            {
+                label: 'Samsung 11',
+                type:'telephone',
+                id :'6'
+            }
+
+        ]
+    },
+    {
+        label: 'Lenovo',
+        items : [
+            {
+                label:'Lenovo 9',
+                type:'telephone',
+                id :'7'
+            },
+            {
+                label: 'Lenovo 10',
+                type:'telephone',
+                id :'8'
+            },
+            {
+                label: 'Lenovo 11',
+                type:'telephone',
+                id :'9'
+            }
+
+        ]
+    },
+    {
+        label: 'One plus',
+        items : [
+            {
+                label:'One plus 9',
+                type:'telephone',
+                id :'10'
+            },
+            {
+                label: 'One plus 10',
+                type:'telephone',
+                id :'11' 
+            },
+            {
+                label: 'One plus 11',
+                
+            }
+
+        ]
+    }
+]);
+
+
+const handleClick = (items) => {
+    router.push(`/Product/${items.type}/${items.id}`);
+}
+
+const router = useRouter();
+
+</script>
