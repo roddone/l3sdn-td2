e966e2bd7d5c509c3023c494c2c53cd6772e2e0a
Alban Pruvost <albanpruvost@MacBook-Pro-de-Alban.local>
2024-03-01 16:51:18 +0100

    fix cart view

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,17 +1,3 @@
-<template>
-  <div>
-    <h1>Contenu du panier</h1>
-    <ul>
-      <li v-for="item in cartItems" :key="item.id">
-        {{ item.nom }} - {{ item.quantity }} x {{ item.prix }}$
-        <button @click="removeFromCart(item.id)">Supprimer</button>
-      </li>
-    </ul>
-    <p>Total: {{ totalPrice }}$</p>
-    <button @click="clearCart">Vider le panier</button>
-  </div>
-</template>
-
 <script>
 import { useCartStore } from '@/stores/cart';
 import { computed } from 'vue';
@@ -40,3 +26,19 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div>
+    <h1>Contenu du panier</h1>
+    <ul>
+      <li v-for="item in cartItems" :key="item.id">
+        {{ item.nom }} - {{ item.quantity }} x {{ item.prix }}$
+        <button @click="removeFromCart(item.id)">Supprimer</button>
+      </li>
+    </ul>
+    <p v-if="cartItems.length === 0">Votre panier est vide.</p>
+    <p v-else>Total : {{ totalPrice }}$</p>
+    <button @click="clearCart" :disabled="cartItems.length === 0">Vider le panier</button>
+  </div>
+</template>
+
